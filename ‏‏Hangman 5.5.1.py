9821870187269627a289489d36acd0beb09406f2
matanboas <37668567+matanboas@users.noreply.github.com>
2020-12-03 16:18:35 +0200

    Delete ‏‏Hangman 5.5.1.py

--- ‏‏Hangman 5.5.1.py
+++
@@ -1,7 +0,0 @@
-def is_valid_input(letter_guessed):
-    letter_guessed = letter_guessed.lower()
-
-    if len(letter_guessed) != 1 or letter_guessed.isalpha() == False:
-        return False
-    else:
-        return True
