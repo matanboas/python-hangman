1a92e53442440a6abaeb2efe7d13f5a0f3ed222c
matanboas <37668567+matanboas@users.noreply.github.com>
2020-12-03 16:19:06 +0200

    Delete ‏‏Hangman 3.5.2.py

--- ‏‏Hangman 3.5.2.py
+++
@@ -1,6 +0,0 @@
-enter_your_word = input("Please enter a word: ")
-#enter your word
-printable_word = "_ " * len(enter_your_word)
-printable_word = printable_word[0:-1]
-#how much characters
-print(printable_word)
